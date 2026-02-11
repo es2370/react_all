11a319d78a95c4a0e12ae94d2446e60bb2b93aff
es2370 <b49062370@gmail.com>
2026-02-11 10:05:00 +0900

    Add files via upload
    260123

---
+++ 260123/react_20250826-main/react_20250826-main/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header id="header" role="banner">
+            <div className="header_inner">
+                <h1 className='header_logo'>
+                    portfolio 
+                    <em>developer</em>
+                </h1>
+                <nav className='header_nav show' role='navigation' aria-label='메인메뉴'>
+                    <ul>
+                        <li><a href="/">Intro</a></li>
+                        <li><a href="/">Skill</a></li>
+                        <li><a href="/">Site</a></li>
+                        <li><a href="/">Port</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </nav>
+                <nav className='header_nav_mobile' id='headerToggle' role='button' tabIndex="0">
+                    <span></span>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
